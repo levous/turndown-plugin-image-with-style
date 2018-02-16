1498f381a864c920af02147f6e39f76e408e7844
Rusty Zarse <rusty@levo.us>
2018-02-16 14:14:14 -0500

    added width and height attributes

--- src/image-with-style.js
+++ src/image-with-style.js
@@ -9,8 +9,8 @@ var rules = {}
 // The information will be appended to the src url as originalSrcUrl?align=[left\center|right]&width=value&height=value
 rules.imageWithSizeAndAlign = {
   regex: {
-    height: /height:\s*(\d+(?:px|em|%){0,1})/i,
-    width: /width:\s*(\d+(?:px|em|%){0,1})/i, // removed the g flag from all cases because this expression was not matching with the flag.  Not sure why...
+    height: /height:\s*(\d+(?:\.\d+)?(?:px|em|%){0,1})/i,
+    width: /width:\s*(\d+(?:\.\d+)?(?:px|em|%){0,1})/i, // removed the g flag from all cases because this expression was not matching with the flag.  Not sure why...
     float: /float:\s*(left|right)/i,
     centered: /^(?=.*display:\s*block)(?=.*margin:auto).*$/i // using positive lookahead to allow for ay order and no need to capture anything
   },
@@ -18,13 +18,15 @@ rules.imageWithSizeAndAlign = {
     // only match if this is an img tag with one of the special treatment style css declaration
     if (node.nodeName !== 'IMG') return false
     var styleAttr = node.getAttribute('style')
+    var widthOrHeight = node.getAttribute('width') + node.getAttribute('height')
 
-    if (styleAttr && (
+    if ((widthOrHeight && widthOrHeight.length > 1) || (
+      styleAttr && (
       this.regex.width.test(styleAttr) ||
       this.regex.height.test(styleAttr) ||
       this.regex.float.test(styleAttr) ||
       this.regex.centered.test(styleAttr)
-    )) {
+    ))) {
       return true
     } else {
       return false
@@ -43,10 +45,25 @@ rules.imageWithSizeAndAlign = {
       height: this.regex.height.exec(attr.style),
       centered: this.regex.centered.test(attr.style)
     }
+
+    const attributeOrStyle = function (attrName) {
+      var val = null
+      var attr = node.getAttribute(attrName)
+      if (match[attrName]) {
+        val = match[attrName][1]
+      } else if (attr && attr.length > 0) {
+        val = attr
+      }
+      return val
+    }
+
+    var height = attributeOrStyle('height')
+    var width = attributeOrStyle('width')
+
     // float and centered will append the same qs key.  If this happens, tough puhtooties.  User error :)
     var querystring = (match.float ? `align=${match.float[1]}&` : '') +
-                      (match.width ? `width=${match.width[1]}&` : '') +
-                      (match.height ? `height=${match.height[1]}&` : '') +
+                      (width ? `width=${width}&` : '') +
+                      (height ? `height=${height}&` : '') +
                       (match.centered ? `align=center&` : '')
 
     var modifiedSrc = attr.src
